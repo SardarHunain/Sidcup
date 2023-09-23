a5858aa804494a66386819475b0ceeac43227145
Hunain Pervaiz <hunainpervaiz487@gmail.com>
2023-09-23 19:56:25 +0500

    added some animations

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var blur= document.querySelector("#cursor-blur")
 
 document.addEventListener("mousemove",function(dets){
     console.log(dets)
-    crsr.style.left = dets.x+"px"
+    crsr.style.left = dets.x+30+"px"
     crsr.style.top = dets.y+"px"
     blur.style.left = dets.x-250+"px"
     blur.style.top = dets.y-250+"px"
@@ -34,6 +34,46 @@ gsap.to("#main",{
         //markers:true,
         start:"top -25%",
         end:"top -70%",
+        scrub:2
+    }
+})
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=3;
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1;
+        crsr.style.border = "0px solid #95C11E";
+        crsr.style.backgroundColor = "#95C11E";
+    })
+})
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        start:"top 60%",
+        end:"top 55%",
+        scrub:3
+    }
+})
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
         scrub:2
     }
 })
