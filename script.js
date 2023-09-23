c3fef291a96f1d37f5fbac27ca19859e54abdb75
Hunain Pervaiz <hunainpervaiz487@gmail.com>
2023-09-23 15:23:29 +0500

    added cursor animations

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var crsr = document.querySelector("#cursor")
+var blur= document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+    console.log(dets)
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x-250+"px"
+    blur.style.top = dets.y-250+"px"
+})
+
+
 gsap.to("#nav",{
     backgroundColor:"#000",
     height:"110px",
@@ -20,8 +32,8 @@ gsap.to("#main",{
         trigger:"#main",
         scroller:"body",
         //markers:true,
-        start:"top -30%",
-        end:"top -80%",
+        start:"top -25%",
+        end:"top -70%",
         scrub:2
     }
 })
